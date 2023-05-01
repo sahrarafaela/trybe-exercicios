00319ea4836ef9a10430e45c81c4a8e5a47072a4
sahrarafaela <sararafaela704@gmail.com>
2023-05-01 12:39:49 -0300

    calculadora console

---
+++ fundamentos/secao-03-introducao-a-javascript/scripts.js
@@ -0,0 +1,16 @@
+// 1 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+let a = 5
+let b = 10
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
